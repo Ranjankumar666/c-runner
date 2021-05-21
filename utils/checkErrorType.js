bf57a46176a83060b552a627799bf808ae463c8c
RanjanKumar666 <typhonazazel@gmail.com>
2021-05-21 11:48:59 +0530

    updated index.js no changes to code added comments to utils

--- utils/checkErrorType.js
+++ utils/checkErrorType.js
@@ -2,6 +2,9 @@
 const { log } = console;
 const chalk = require('chalk');
 
+/**
+ * @param {Error} error
+ */
 module.exports.checkErrorType = function checkErrorType(error) {
     if (error.errno === -4058) {
         log(chalk.red`Path doesn't exists : ${options.run}`);
